f72c37e98b92be3a163fd4d1bc33cf4f4c107443
Jason Reilly <jdr0887@renci.org>
2013-05-16 12:50:23 -0400

    adding new test

--- unc-kure-service/src/test/java/org/renci/gate/plugin/kure/LookupMetricsTest.java
+++ unc-kure-service/src/test/java/org/renci/gate/plugin/kure/LookupMetricsTest.java
@@ -13,14 +13,134 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 import org.junit.Test;
 import org.renci.gate.GlideinMetric;
+import org.renci.jlrm.Queue;
+import org.renci.jlrm.Site;
 import org.renci.jlrm.lsf.LSFJobStatusInfo;
 import org.renci.jlrm.lsf.ssh.LSFSSHJob;
+import org.renci.jlrm.lsf.ssh.LSFSSHLookupStatusCallable;
 
 public class LookupMetricsTest {
 
+    @Test
+    public void testLookupMetricsForReal() {
+        List<LSFSSHJob> jobCache = new ArrayList<LSFSSHJob>();
+
+        Map<String, GlideinMetric> metricsMap = new HashMap<String, GlideinMetric>();
+
+        try {
+            
+            Site site = new Site();
+            site.setName("Kure");
+            site.setProject("TCGA");
+            site.setUsername("rc_renci.svc");
+            site.setSubmitHost("biodev2.its.unc.edu");
+            site.setMaxTotalPending(4);
+            site.setMaxTotalRunning(4);
+            Map<String, Queue> queueInfoMap = new HashMap<String, Queue>();
+            Queue queue = new Queue();
+            queue.setMaxJobLimit(10);
+            queue.setMaxMultipleJobsToSubmit(2);
+            queue.setName("pseq_prod");
+            queue.setWeight(1D);
+            queue.setPendingTime(1440);
+            queue.setRunTime(5760);
+            queueInfoMap.put("pseq_prod", queue);
+            site.setQueueInfoMap(queueInfoMap);
+            
+            LSFSSHLookupStatusCallable callable = new LSFSSHLookupStatusCallable(jobCache, site);
+            Set<LSFJobStatusInfo> jobStatusSet = Executors.newSingleThreadExecutor().submit(callable).get();
+
+            // get unique list of queues
+            Set<String> queueSet = new HashSet<String>();
+            if (jobStatusSet != null && jobStatusSet.size() > 0) {
+                for (LSFJobStatusInfo info : jobStatusSet) {
+                    queueSet.add(info.getQueue());
+                }
+                for (LSFSSHJob job : jobCache) {
+                    queueSet.add(job.getQueueName());
+                }
+            }
+
+            Set<String> alreadyTalliedJobIdSet = new HashSet<String>();
+
+            if (jobStatusSet != null && jobStatusSet.size() > 0) {
+                for (LSFJobStatusInfo info : jobStatusSet) {
+                    if (metricsMap.containsKey(info.getQueue())) {
+                        continue;
+                    }
+                    if (!"glidein".equals(info.getJobName())) {
+                        continue;
+                    }
+                    metricsMap.put(info.getQueue(), new GlideinMetric(0, 0, info.getQueue()));
+                    alreadyTalliedJobIdSet.add(info.getJobId());
+                }
+
+                for (LSFJobStatusInfo info : jobStatusSet) {
+
+                    if (!"glidein".equals(info.getJobName())) {
+                        continue;
+                    }
+
+                    GlideinMetric metric = metricsMap.get(info.getQueue());
+                    switch (info.getType()) {
+                        case PENDING:
+                            metric.setPending(metric.getPending() + 1);
+                            break;
+                        case RUNNING:
+                            metric.setRunning(metric.getRunning() + 1);
+                            break;
+                    }
+                }
+            }
+
+            Iterator<LSFSSHJob> jobCacheIter = jobCache.iterator();
+            while (jobCacheIter.hasNext()) {
+                LSFSSHJob nextJob = jobCacheIter.next();
+                for (LSFJobStatusInfo info : jobStatusSet) {
+
+                    if (!nextJob.getName().equals(info.getJobName())) {
+                        continue;
+                    }
+
+                    if (!alreadyTalliedJobIdSet.contains(nextJob.getId()) && nextJob.getId().equals(info.getJobId())) {
+                        GlideinMetric metric = metricsMap.get(info.getQueue());
+                        switch (info.getType()) {
+                            case PENDING:
+                                metric.setPending(metric.getPending() + 1);
+                                break;
+                            case RUNNING:
+                                metric.setRunning(metric.getRunning() + 1);
+                                break;
+                            case EXIT:
+                            case UNKNOWN:
+                            case ZOMBIE:
+                            case DONE:
+                                jobCacheIter.remove();
+                                break;
+                            case SUSPENDED_BY_SYSTEM:
+                            case SUSPENDED_BY_USER:
+                            case SUSPENDED_FROM_PENDING:
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        
+        
+        
+
+    }
+    
     @Test
     public void testLookupMetrics() {
         List<LSFSSHJob> jobCache = new ArrayList<LSFSSHJob>();
