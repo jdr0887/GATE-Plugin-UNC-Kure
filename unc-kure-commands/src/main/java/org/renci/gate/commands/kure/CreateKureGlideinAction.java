b659611a0c9a5e81993f43377cca2c7e4b3427fd
Jason Reilly <jdr0887@renci.org>
2013-12-19 15:38:55 -0500

    fixing option names

--- unc-kure-commands/src/main/java/org/renci/gate/commands/kure/CreateKureGlideinAction.java
+++ unc-kure-commands/src/main/java/org/renci/gate/commands/kure/CreateKureGlideinAction.java
@@ -14,16 +14,16 @@ import org.renci.jlrm.lsf.ssh.LSFSSHSubmitCondorGlideinCallable;
 @Command(scope = "unc-kure", name = "create-glidein", description = "Create Glidein")
 public class CreateKureGlideinAction extends AbstractAction {
 
-    @Option(name = "username", required = true, multiValued = false)
+    @Option(name = "--username", required = true, multiValued = false)
     private String username;
 
-    @Option(name = "submitHost", required = true, multiValued = false)
+    @Option(name = "--submitHost", required = true, multiValued = false)
     private String submitHost;
 
-    @Option(name = "queueName", required = true, multiValued = false)
+    @Option(name = "--queueName", required = true, multiValued = false)
     private String queueName;
 
-    @Option(name = "collectorHost", required = true, multiValued = false)
+    @Option(name = "--collectorHost", required = true, multiValued = false)
     private String collectorHost;
 
     public CreateKureGlideinAction() {
